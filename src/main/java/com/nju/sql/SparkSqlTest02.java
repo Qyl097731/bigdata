df117dffdeecbfa040e1c8b9e10353d7df729a54
邱依良 <553579048@qq.com>
2022-11-16 20:07:13 +0800

    spark 学习

--- src/main/java/com/nju/sql/SparkSqlTest02.java
+++ src/main/java/com/nju/sql/SparkSqlTest02.java
@@ -4,32 +4,27 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
 
 /**
+ * udf 自定义函数
+ *
  * @date:2022/11/16 12:27
  * @author: qyl
  */
-public class SparkSqlTest01 {
+public class SparkSqlTest02 {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setMaster("local").setAppName("sql");
         SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
 
         Dataset<User> df = spark.read().json("data/user.json").as(Encoders.bean(User.class));
-        df.show();
-
         // DataFrame => SQL
 
         df.createOrReplaceTempView("user");
 
-        spark.sql("select * from user where age = 13").show();
-        spark.sql("select username from user").show();
-        spark.sql("select avg(age) from user").show();
+        spark.udf().register("prefixName", (s) -> "name: " + s, DataTypes.StringType);
 
-        // DataFrame => DSL
-        df.select("age", "username").show();
-
-        // DataSet
-        df.filter(df.col("username").equalTo("张三")).show();
+        spark.sql("select age,prefixName(username) as name from user where age = 13").show();
 
     }
 
