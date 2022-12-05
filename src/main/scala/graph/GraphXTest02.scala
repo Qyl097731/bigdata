2a0de4a0a497c5f7db2346a939a645c638f201ed
邱依良 <553579048@qq.com>
2022-12-05 23:12:07 +0800

    graphX 简单使用

--- src/main/scala/graph/GraphXTest02.scala
+++ src/main/scala/graph/GraphXTest02.scala
@@ -67,7 +67,46 @@ object GraphXTest02 {
     graph.mapVertices {
       case (id, (name, age)) => (id, (name, age + 10))
     }.vertices.collect.foreach(println)
+
+    println("边的转换操作，边的属性*2")
+    graph.mapEdges(e => e.attr * 2).edges.collect.foreach(println)
+
+    /**
+     * 结构操作
+     */
+    println("结构操作")
+    println("顶点 > 30 的子图")
+    val subGraph = graph.subgraph(vpred = (id, vd) => vd._2 >= 30)
+
+    println("子图所有顶点")
+    subGraph.vertices.collect.foreach(println)
+
+    println("子图所有边")
+    subGraph.edges.collect.foreach(println)
+
+    /**
+     * 连接操作
+     */
+    println("连接操作")
+    println("***********************************")
+    val idDegrees = graph.inDegrees
+    // 创建一个新图，顶点VD的数据类型为User，并从graph做类型转换
+    val initialUserGraph = graph.mapVertices {
+      case (id, (name, age)) => User(name, age, 0, 0)
+    }
+    val userGraph = initialUserGraph.outerJoinVertices(initialUserGraph.inDegrees) {
+      case (id, u, inDegOut) => User(u.name, u.age, inDegOut.getOrElse(0), u.outDeg)
+    }.outerJoinVertices(initialUserGraph.outDegrees) {
+      case (id, u, outDegout) => User(u.name, u.age, u.inDeg, outDegout.getOrElse(0))
+    }
+    println("连接图的属性")
+    userGraph.vertices.foreach(println)
+    println("出度和入读相同的人员")
+    userGraph.vertices.filter {
+      case (id, u) => u.inDeg == u.outDeg
+    }.collect.foreach(println)
   }
-
-
 }
+
+case class User(name: String, age: Int, inDeg: Int, outDeg: Int)
+
